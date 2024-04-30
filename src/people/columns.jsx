03ef6c111ce2b86428d8532dcce511cd8b9219b6
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-04-30 20:06:11 +0300

    Added an action button to the table rows through the columns file. Then inside the action button set up a dropdown menu with option to copy user's first name and option to delete the whole row

--- src/people/columns.jsx
+++ src/people/columns.jsx
@@ -1,3 +1,14 @@
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "../components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export const columns = [
   {
     accessorKey: "id",
@@ -25,6 +36,35 @@ export const columns = [
     cell: ({ row }) => {
       const date_of_birth = row.getValue("date_of_birth");
       return <div className="font-medium">{date_of_birth}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const person = row.original;
+      const personId = person.id;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {
+                navigator.clipboard.writeText(person.first_name.toString());
+              }}
+            >
+              Copy person's name
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-red-400">
+              Delete row
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
