9cd37a2cd452b1f292003f8c73c68add6ecaec67
My pc <mypc@TamaraMacBook-Air.local>
2024-05-11 18:54:30 +0300

    Added a sheet that can be potentially used to add more info/data to the table

--- src/people/page.jsx
+++ src/people/page.jsx
@@ -3,6 +3,18 @@ import DataTable from "./data-table";
 import { people } from "../people";
 import { columns } from "./columns";
 import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { Plus } from "lucide-react";
 import { expenditure } from "../expenditure";
 
@@ -18,10 +30,41 @@ const Expenditure = () => {
             List of trips made by your vehicles
           </h4>
         </div>
-        <Button className="bg-[#013941] hover:bg-[#053030]" size="lg">
-          <Plus size={15} className="mr-3" />
-          Add Data
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="bg-[#013941] hover:bg-[#053030]" size="lg">
+              <Plus size={15} className="mr-3" />
+              Add Data
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-1/2">
+            <SheetHeader>
+              <SheetTitle>Edit profile</SheetTitle>
+              <SheetDescription>
+                Make changes to your profile here. Click save when you're done.
+              </SheetDescription>
+            </SheetHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input id="name" value="Pedro Duarte" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input id="username" value="@peduarte" className="col-span-3" />
+              </div>
+            </div>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button type="submit">Save changes</Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
       </div>
       <DataTable columns={columns} data={expenditure} />
     </>
