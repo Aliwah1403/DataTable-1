c89d630a7610f1c2408e2c4c13e05a08effb4189
Curtis Aliwah <aliwahcurtis5@gmail.com>
2024-06-14 15:40:56 +0300

    refactor: Changed the faceted filtering ability and now can combine more than one filter on the table

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import {rankItem} from '@tanstack/match-sorter-utils'
+import { rankItem } from "@tanstack/match-sorter-utils";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
@@ -18,10 +18,23 @@ export const getDropDownValues = (data, selector) => {
   return optionsArray;
 };
 
-export const includesStringFilterFn = (row, columnId, filterValue) => {
-  const value = String(row.getValue(columnId));
-  const filterString = String(filterValue).toLowerCase();
-  return value.toLowerCase().includes(filterString);
+export const includesStringFilterFn = (row, columnId, filterValues) => {
+  // Suitable for only one filter at a time (change argument to filterValue)
+  
+  // const value = String(row.getValue(columnId));
+  // const filterString = String(filterValue).toLowerCase();
+  // return value.toLowerCase().includes(filterString);
+
+
+
+  if (!Array.isArray(filterValues) || filterValues.length === 0) {
+    return true; // If no filters are selected, include all rows
+  }
+
+  const rowValue = String(row.getValue(columnId)).toLowerCase();
+  return filterValues.some((filterValue) =>
+    rowValue.includes(String(filterValue).toLowerCase())
+  );
 };
 
 includesStringFilterFn.autoRemove = (val) => !val;
